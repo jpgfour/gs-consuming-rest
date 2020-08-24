e22acf4f91d3f98e8b510747037688e9e2b36c69
jpgfour <jeremy.grise@gmail.com>
2020-08-24 17:32:03 -0500

    got to run by editing build.gradle to match the text on web.  experimenting with generating quotes pulled from API as JSON

--- initial/src/main/java/com/example/consumingrest/ConsumingRestApplication.java
+++ initial/src/main/java/com/example/consumingrest/ConsumingRestApplication.java
@@ -1,13 +1,43 @@
 package com.example.consumingrest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.context.request.RequestContextHolder;
+
+import javax.servlet.http.HttpSession;
 
 @SpringBootApplication
 public class ConsumingRestApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(ConsumingRestApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConsumingRestApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+
+	@Bean
+	@ResponseBody
+	@RequestMapping(value = "")
+	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+		return args -> {
+			Quote quote = restTemplate.getForObject(
+					"https://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+			log.info(quote.toString());
+
+			System.out.println(quote.toString());
+		};
+	}
 }
